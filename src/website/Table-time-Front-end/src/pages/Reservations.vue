802a0626d325af5008e9e0a6ab8b2605e6563fb1
Johndaboy <145539556+Johndaboy@users.noreply.github.com>
2025-11-21 15:37:06 +0100

    reservation form and styling

--- src/website/Table-time-Front-end/src/pages/Reservations.vue
+++ src/website/Table-time-Front-end/src/pages/Reservations.vue
@@ -1,5 +1,65 @@
+<script>
+import NavBar from '../components/NavBar.vue';
+
+export default {
+  components: {
+    NavBar,
+  },
+};
+
+// Script of making a request to the api
+document.addEventListener('DOMContentLoaded', () => {
+  const form = document.querySelector('form');
+
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const date = form.date.value;
+    const time = form.time.value;
+    const duration = form.duration.value;
+    const amountPeople = form.amountPeople.value;
+
+    fetch('http://localhost:8080/reservations', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        date,
+        time,
+        duration,
+        amountPeople,
+      }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log('Success:', data);
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+      });
+  });
+});
+</script>
+
 <template>
-    <main>
-        asdassdas
-    </main>
+      <NavBar />
+  <main class="flex justify-center">
+    <section>
+      <h2>Reservation</h2>
+      <div>
+        <form>
+            <label for="date"><p>Date</p></label>
+            <input type="date" name="date" placeholder="Date" required />
+            <label for="time"><p>Time of day</p></label>
+            <input type="time" name="time" placeholder="Time" required />
+            <label for="duration"><p>Duration of the reservation</p></label>
+            <input type="time" name="duration" placeholder="Duration" required />
+            <label for="amountPeople"><p>How many people are you taking with you?</p></label>
+            <input type="number" name="amountPeople" placeholder="Amount of people" value="1" required />
+            <input type="submit" value="Make reservation" />
+        </form>
+      </div>
+    </section>
+  </main>
 </template>
