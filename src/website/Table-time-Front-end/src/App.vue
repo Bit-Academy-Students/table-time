b19823f4b5b2101868233d01909118d467787b81
alexander <alexanderzoet@gmail.com>
2025-11-21 11:53:58 +0100

    almost

--- src/website/Table-time-Front-end/src/App.vue
+++ src/website/Table-time-Front-end/src/App.vue
@@ -1,12 +1,18 @@
 <script setup>
+import Footer from './components/Footer.vue';
+import GuestText from './components/homePageText/GuestText.vue';
+import RestaurantOwnersText from './components/homePageText/RestaurantOwnersText.vue';
 import NavBar from './components/NavBar.vue';
+import { ref } from 'vue';
+
+const active = ref('Guests');
 
 </script>
 
 <template>
     <NavBar />
     <main class="flex flex-col items-center pt-[100px]">
-        <section class="flex flex-row justify-center w-[80%] mb-[300px]">
+        <section class="flex flex-row justify-center w-[80%] mb-[100px]">
             <div class="w-[50%]">
                 <h2>Table Time</h2>
                 <p>Table Time is een moderne webapp waarmee klanten eenvoudig tafels kunnen reserveren, aanpassen of
@@ -22,65 +28,21 @@ import NavBar from './components/NavBar.vue';
         </section>
         <h1 class="flex justify-center">Hoe werkt het</h1>
         <div class="flex flex-row gap-5 bg-white p-2 rounded-[10px] mb-[100px]">
-            <button>Gasten</button>
-            <button>Restaurant­eigenaren</button>
+            <button
+            :class="{ 'bg-[#FF8000]' : active === 'Guests' }"
+            @click="active = 'Guests'">Gasten</button>
+            <button
+            :class="{ 'bg-[#FF8000]' : active === 'restaurantOwners' }"
+            @click="active = 'restaurantOwners'">Restaurant­eigenaren</button>
         </div>
-        <section class="flex flex-col justify-center w-[80%]">
-            <div class="w-full  border-t-4 border-[#FF8000] flex flex-row justify-between items-center gap-10 p-10">
-                <div class="flex justify-between gap-10">
-                    <h2 class="--h2Li">
-                        1.</h2>
-                    <div class="flex flex-col justify-center">
-                        <h2>Kies je restaurant!</h2>
-                        <p>Vind jouw favoriete plek in de lijst!</p>
-                    </div>
-                </div>
-                    <img src="./assets/img/choosingRestaurant.png" class="rounded-full" alt="">
-                </div>
-                <div class="w-full   flex flex-row justify-between items-center gap-10">
-                    <img src="./assets/img/choosingDateAndTime.png" alt="">
-                    <div class="flex justify-between">
-                        <div class="flex flex-col justify-center">
-                            <h2>Selecteer datum & tijd</h2>
-                            <p>Zie direct welke tafels vrij zijn.</p>
-                        </div>
-                        <h2 class="--h2Li">
-                            2.</h2>
-                    </div>
-                </div>
-                <div class="w-full  flex flex-row justify-between items-center gap-10">
-                    <div class="flex justify-between">
-                        <h2 class="--h2Li">
-                            3.</h2>
-                        <div class="flex flex-col justify-center">
-                            <h2>Vul je gegevens in</h2>
-                            <p>Geef aan met hoeveel personen je komt en voeg speciale wensen toe.</p>
-                        </div>
-                    </div>
-                    <img src="./assets/img/fillingIn.png" class="rounded-full" alt="">
-                </div>
-                <div class="w-full flex flex-row justify-between items-center gap-10">
-                    <img src="./assets/img/confirmEmail.png" class="rounded-full" alt="">
-                    <div class="flex justify-between">
-                        <div class="flex flex-col justify-end">
-                            <h2>Bevestig</h2>
-                            <p>Ontvang direct een bevestiging per e-mail.</p>
-                        </div>
-                        <h2 class="--h2Li">
-                            4.</h2>
-                    </div>
-                </div>
-                <div class="w-full flex flex-row justify-between items-center gap-10 p-4">
-                    <div class="flex justify-between gap-10">
-                    <h2 class="--h2Li">
-                        5.</h2>
-                    <div class="flex flex-col justify-center">
-                        <h2>Beheer zelf</h2>
-                        <p>Bekijk, wijzig of annuleer eenvoudig jouw reservering.</p>
-                    </div>
-                </div>
-                    <img src="./assets/img/confirmEmail.png" class="rounded-full" alt="">
-                </div>
+        <section v-if="active === 'Guests'"
+        id="Guests" class="flex flex-col justify-center w-[80%]">
+        <GuestText />
+        </section>
+        <section v-if="active === 'restaurantOwners'"
+         id="restaurantOwners" class="flex flex-col justify-center w-[80%]">
+         <RestaurantOwnersText  />
         </section>
     </main>
+    <Footer />
 </template>
---
+++ src/website/Table-time-Front-end/src/components/Footer.vue
@@ -0,0 +1,23 @@
+<template>
+    <footer class="bg-white border-t-4 border-[#FF8000] flex flex-col justify-center p-4 mt-4">
+        <div class="flex flex-row justify-around p-4 mt-4">
+            <ul>
+                <li><img src="../assets/img/logo TableTImes.png" class="w-[300px]" alt=""></li>
+            </ul>
+            <ul>
+                <li>
+                    <h2>Pages</h2>
+                </li>
+                <li><a href="/">Home</a></li>
+                <li><a href="/">Reserveren</a></li>
+                <li><a href="/">About</a></li>
+                <li><a href="/">Contact</a></li>
+                <li><a href="/">Login/registreren</a></li>
+            </ul>
+        </div>
+
+        <div class="border-t-2 border-[#FF8000]">
+            <p class="flex justify-center items-center">2025 Team Unc B.V.</p>
+        </div>
+    </footer>
+</template>
