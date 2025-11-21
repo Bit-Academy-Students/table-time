85fce88ed7f1ae85f698bd9fa0dd806c0d8de419
alexander <alexanderzoet@gmail.com>
2025-11-21 10:54:56 +0100

    added routing

---
+++ src/website/Table-time-Front-end/src/router/index.js
@@ -0,0 +1,19 @@
+import { createRouter, createWebHistory } from 'vue-router';
+
+// Pagina-componenten importeren
+import Home from '../Home.vue';
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: Home,
+  },
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+export default router;
